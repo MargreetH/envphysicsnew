8fc04ab5abf497f08f48c0db8b2c9de3f31d715c
MargreetH <0margreet@gmail.com>
2018-04-23 14:06:20 +0200

    Changes exercise4

--- Problemset2/exercise4.m
+++ Problemset2/exercise4.m
@@ -10,10 +10,21 @@ meanU = mean(U);
 varU = var(U);
 
 [k,labda,~] = findWeibull(varU,meanU,0.00000001);
+figure;
 delta_u = 0.01;
 u1 = 0.1:delta_u:30;
 PDFWeibull = weibullVector(u1,labda,k);
-plot(u1,PDFWeibull);
+hold on;
+%Plot the histogram
+h = histogram(U, 'Normalization','pdf');
+
+plot(u1,PDFWeibull,'LineWidth',2);
+xlabel('u[m/s]')
+ylabel('PDF')
+title('Determined weibull distribution for the KNMI dataset, using u at a height of 80 m')
+set(gca,'FontSize',10) % make fontsize bigger
+set(gcf,'color','w'); % Set bg color to white
+hold off;
 
 %% b)
 % Compute annual mean wind power
@@ -47,5 +58,24 @@ plot(u1,PDFWeibullgeo);
 PDFWeibullOperationalgeo = PDFWeibullgeo(cutInIndex:cutOutIndex);
 annualMeanWindPowergeo = sum(2.*rho .* u1Operational.^3 .* A_T .* a.*(1-a).^2 .* PDFWeibullOperationalgeo .* delta_u);
 
-differenceTheoreticAndActual = abs(annualMeanWindPower - annualMeanWindPowergeo)
+differenceTheoreticAndActual = abs(annualMeanWindPower - annualMeanWindPowergeo);
+
+%% Determine maximum wake effect
+alpha = 0.082;
+a_c = 1/3;
+gamma = sqrt((1-a_c)./(1-2 *a_c));
+r = 1- 2.* a_c ./ (1+ alpha .* 500./(gamma .* Rblade)).^2 ;
+
+Usecondturbine = U * r;
+mean2 = mean(Usecondturbine);
+var2 = var(Usecondturbine);
+[k2,labda2,~] = findWeibull(var2,mean2,0.00000001);
+delta_u = 0.01;
+u1 = 0.1:delta_u:30;
+PDFWeibull2 = weibullVector(u1,labda2,k2);
+plot(u1,PDFWeibull2);
+cutInIndex = find(u1==4);
+cutOutIndex = find(u1==25);
+PDFWeibullOperational2 = PDFWeibull2(cutInIndex:cutOutIndex);
+annualMeanWindPower2 = sum(2.*rho .* u1Operational.^3 .* A_T .* a.*(1-a).^2 .* PDFWeibullOperational2 .* delta_u);
 
